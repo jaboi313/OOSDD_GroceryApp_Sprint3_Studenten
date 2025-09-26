56ca3d0a14fd1be1d178fabd918a1db3a8314bcd
Kelvin Hollander <47366702+jaboi313@users.noreply.github.com>
2025-09-26 13:22:30 +0200

    Project files addon

---
+++ Grocery.UnitTests/Mocks/MockClientRepository.cs
@@ -0,0 +1,45 @@
+﻿using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.UnitTests.Mocks
+{
+    internal class MockClientRepository : IClientRepository
+    {
+        private readonly List<Client> clientList;
+
+        public MockClientRepository()
+        {
+            clientList = [
+                new Client(1, "M.J. Curie", "user1@mail.com", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08="),
+                new Client(2, "H.H. Hermans", "user2@mail.com", "dOk+X+wt+MA9uIniRGKDFg==.QLvy72hdG8nWj1FyL75KoKeu4DUgu5B/HAHqTD2UFLU="),
+            ];
+        }
+
+        public Client? Get(string email)
+        {
+            Client? client = clientList.FirstOrDefault(c => c.EmailAddress.Equals(email));
+            return client;
+        }
+
+        public Client? Get(int id)
+        {
+            Client? client = clientList.FirstOrDefault(c => c.Id == id);
+            return client;
+        }
+
+        public void Add(Client client)
+        {
+            clientList.Add(client);
+        }
+
+        public List<Client> GetAll()
+        {
+            return clientList;
+        }
+    }
+}
